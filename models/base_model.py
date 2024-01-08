addd55fa2e3f0b3a3ebe9d136ac8c53cd0fb7591
GeoEM178 <geoem178@gmail.com>
2024-01-08 15:44:33 +0200

    Start the project and setup directories structure :) && Add BaseModel and its unit testing & Test the model and all is fine :)

---
+++ models/base_model.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Module: base_model.py
+This is the "base model" module.
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+  """BaseModel is a base for all the classes - will be inherited
+
+  Attributes:
+    id(str): the unique id of the instance(user)
+    created_at: when the instance created
+    updated_at: when the instance updated
+
+  Methods:
+    __str__: return class name, instance id, and the dictionary
+    save(self): updates instance updated_at
+    to_dict(self): returns the dictionary keys and values of the object
+
+  """
+  def __init__(self):
+    """Instance Constructor initialization
+
+    Args:
+      None: there is no arguments until now
+
+    """
+    self.id = str(uuid.uuid4())
+
+    self.created_at = datetime.utcnow()
+    self.updated_at = datetime.utcnow()
+
+  def save(self):
+    """
+    Updates the updated_at attribute:
+    with new current date and time
+    """
+    self.updated_at = datetime.utcnow()
+
+  def to_dict(self):
+    """
+    Method returns a dictionary with all 
+    keys and values of the __dict__
+    """
+    instance_dictionary = self.__dict__.copy()
+    instance_dictionary['__class__'] = self.__class__.__name__
+    instance_dictionary['created_at'] = self.created_at.isoformat()
+    instance_dictionary['updated_at'] = self.updated_at.isoformat()
+    return instance_dictionary
+  
+  def __str__(self):
+    """
+    Returns string representation of the class and instance
+    """
+    return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+  
+# TODO: For testing - will be deleted
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
